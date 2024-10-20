275700c36af8f995ac0b39939275de3bb155cec6
Sudeesh Reddy <gvarmasubbu39@gmail.com>
2024-10-20 19:22:35 +0530

    change in db

--- db.py
+++ db.py
@@ -1,13 +1,21 @@
 import sqlite3
 import uuid
+import os
 
 
 def startDb():
-    db = sqlite3.connect("cnad.db")
+    db_file = "cnad.db"
+
+    if os.path.exists(db_file):
+        os.remove(db_file)
+
+    db = sqlite3.connect(db_file)
     cursor = db.cursor()
     cursor.execute("PRAGMA foreign_keys = ON;")
+
     create_users_uploads(cursor)
     db.commit()
+    db.close()
 
 def create_users_uploads(cursor):
     cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='user_upload'")
@@ -32,12 +40,14 @@ def add_user_uploads(data):
                       VALUES (?, ?, ?, ?, ?)''',
                    (user_id, data['email'], data['image_path'], data['transcript'], data['text_path']))
     db.commit()
+    db.close()
 
 def get_records_by_email(email):
     db = sqlite3.connect("cnad.db")
     cursor = db.cursor()
     cursor.execute('''SELECT * FROM user_upload WHERE email = ?''', (email,))
     records = cursor.fetchall()
+    db.close()
 
     records_list = []
     for record in records:
