43c87bb6c1181d01422028c75d386b4827571054
Sudeesh Reddy <gvarmasubbu39@gmail.com>
2024-11-10 23:21:15 +0530

    allowed types

--- helpers.py
+++ helpers.py
@@ -1,4 +1,4 @@
 def allowed_file(filename):
-    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'bmp', 'tiff'}
+    ALLOWED_EXTENSIONS = {'png', 'jpg'}
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
