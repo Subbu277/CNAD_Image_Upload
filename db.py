15b8b5bc49372cf6a49a8317780a6ea2a3b35291
Sudeesh Reddy <gvarmasubbu39@gmail.com>
2024-11-12 03:44:19 +0530

    google.cloud.sql.connector

--- db.py
+++ db.py
@@ -4,29 +4,41 @@ import sqlalchemy
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import text
 from google_cloud import  download_file
+from google.cloud.sql.connector import Connector, IPTypes
+import pymysql
 
+def connect_with_connector() -> sqlalchemy.engine.base.Engine:
 
-def connect_tcp_socket() -> sqlalchemy.engine.base.Engine:
-    db_host = os.environ["INSTANCE_HOST"]
+    instance_connection_name = os.environ[
+        "INSTANCE_CONNECTION_NAME"
+    ]
     db_user = os.environ["DB_USER"]
     db_pass = os.environ["DB_PASS"]
     db_name = os.environ["DB_NAME"]
-    db_port = os.environ["DB_PORT"]
+
+    ip_type = IPTypes.PRIVATE if os.environ.get("PRIVATE_IP") else IPTypes.PUBLIC
+
+    connector = Connector(ip_type)
+
+    def getconn() -> pymysql.connections.Connection:
+        conn: pymysql.connections.Connection = connector.connect(
+            instance_connection_name,
+            "pymysql",
+            user=db_user,
+            password=db_pass,
+            db=db_name,
+        )
+        return conn
 
     pool = sqlalchemy.create_engine(
-        sqlalchemy.engine.url.URL.create(
-            drivername="mysql+pymysql",
-            username=db_user,
-            password=db_pass,
-            host=db_host,
-            port=db_port,
-            database=db_name,
-        ),
+        "mysql+pymysql://",
+        creator=getconn,
+        # ...
     )
     return pool
 
 def add_user_upload_record(data) -> None:
-    engine = connect_tcp_socket()
+    engine = connect_with_connector()
 
     Session = sessionmaker(bind=engine)
     session = Session()
@@ -55,7 +67,7 @@ def add_user_upload_record(data) -> None:
         session.close()
 
 def fetch_user_uploads_by_email(email: str):
-    engine = connect_tcp_socket()
+    engine = connect_with_connector()
 
     Session = sessionmaker(bind=engine)
     session = Session()
